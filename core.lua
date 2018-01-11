d94b84556cd403f48fcfa257e841f99e844ed69b
blooblahguy <blooblahguy@gmail.com>
2018-01-10 19:24:52 -0700

    adding coords

--- core.lua
+++ core.lua
@@ -309,3 +309,63 @@ Minimap.buttonFrame:SetScript("OnUpdate",function(self,elapsed)
 		end
 	end
 end)
+
+-- thank you to xcoords
+local bdCoords = CreateFrame("frame", nil, WorldMapFrame)
+bdCoords.text = bdCoords:CreateFontString(nil)
+bdCoords.text:SetFont(bdCore.media.font, 14)
+bdCoords.text:SetAllPoints()
+bdCoords:SetFrameLevel("OVERLAY")
+bdCoords:SetPoint("BOTTOM", WorldMapFrame, "BOTTOM")
+bdCoords:SetScript("OnUpdate", function()
+	-- Player
+	local pX, pY, Nick, cX, cY, cursor, left, bottom, height, width, scale
+	pX, pY = GetPlayerMapPosition("player");
+	pX = pX*100
+	pY = pY*100
+	pX = math.floor(pX*10)/10
+	pY = math.floor(pY*10)/10
+	if pX == 0.0 or pY == 0.0 then
+		Nick = "N/A";
+	else
+		Nick = UnitName("player")
+		pX = string.format("%.1f", pX)
+		pY = string.format("%.1f", pY)
+		Nick = Nick .. ": |cffffffff" .. pX .. ", " .. pY;
+	end
+
+	-- Cursor
+	width, height, scale = WorldMapDetailFrame:GetWidth(), WorldMapDetailFrame:GetHeight(), WorldMapDetailFrame:GetEffectiveScale();
+	cX, cY = WorldMapDetailFrame:GetCenter()
+	left, bottom = cX - width / 2, cY + height /2;
+
+	cX, cY = GetCursorPosition();
+	cX, cY = (cX / scale - left) / width * 100, (bottom - cY / scale) / height * 100;
+	
+	if cX < 0 or cX > 100 or cY < 0 or cY > 100 then
+		cursor = "N/A"
+	else
+		--cX = cX*100
+		--cY = cY*100
+		cX = math.floor(cX*10)/10
+		cY = math.floor(cY*10)/10
+		cX = string.format("%.1f", cX)
+		cY = string.format("%.1f", cY)
+		cursor = "Cursor: |cffffffff" .. cX .. ", " .. cY;
+	end
+
+	self.text:SetText(Nick .. "|r  -  " .. cursor .. "|r");
+end)
+
+
+
+function coordsResize()
+	if WORLDMAP_SETTINGS.size == WORLDMAP_WINDOWED_SIZE then
+		bdCoords:SetPoint("BOTTOM", WorldMapFrame, "BOTTOM", 0, 20)
+	else 
+		bdCoords:SetPoint("BOTTOM", WorldMapFrame, "BOTTOM", 0, 10)
+	end
+end
+
+WorldMapFrameSizeUpButton:HookScript("OnClick", coordsResize)
+WorldMapFrame:HookScript("OnShow", coordsResize)
